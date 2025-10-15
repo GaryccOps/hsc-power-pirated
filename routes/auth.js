d859ca9863945a2c1209fab55f6f6821488de37d
GaryccOps <linchen.chen2020@gmail.com>
2025-10-15 20:29:00 +1100

    restrucutre project structure

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const loginController = require('../controllers/loginController');
-const signupController = require('../controllers/signupController');
+const loginController = require('../controllers/login');
+const signupController = require('../controllers/signup');
 
 // POST /auth/signup - Handle signup form submission
-router.post('/signup', signupController.signup);
+// router.post('/signup', signupController.signup);
 
 // POST /auth/login
 router.post('/login', loginController.login);
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const bookController = require('../controllers/bookController');
 const verifyJWT = require('../middlewares/verifyJWT');
 
 router.use(verifyJWT);
@@ -9,7 +8,5 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-// POST /books
-router.post('/add_book', bookController.addBook);
 
 module.exports = router;
